dc3fc5517357bb2060bf72da13f1ec90ee8ded34
victo <victo@Victor>
2025-04-06 21:03:32 -0300

    Commit Inicial

---
+++ src/main/java/com/meetime/casehubspot/CasehubspotApplication.java
@@ -0,0 +1,13 @@
+package com.meetime.casehubspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasehubspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasehubspotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meetime/casehubspot/CasehubspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetime.casehubspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasehubspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
